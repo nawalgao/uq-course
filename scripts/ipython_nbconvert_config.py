64b1bc94568ea93ac963721c47988edb67650d27
Ilias Bilionis <ebilionis@gmail.com>
2016-01-04 17:36:34 +0200

    Removed elements that are not required.

--- scripts/ipython_nbconvert_config.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-
-#Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['*.ipynb']
-c.NbConvertApp.export_format = 'html'
-c.Exporter.template_file = 'thmsInNb'
